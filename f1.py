c556be64d132c0a091b8d8d2fbebaf6a53f7f20f
hayim <ihimihal@gmail.com>
2026-02-11 17:51:41 +0200

    add source_ip

--- f1.py
+++ f1.py
@@ -4,3 +4,9 @@ def read(path):
     with open(path,'r') as file:
             listi_read =[list(line.split(",")) for line in file]
     return listi_read
+
+def source_ip (ips):
+    list_ip =[ip for ip in ips if not ip[1].startswith("10") and not ip[1].startswith("192.168")]
+    return list(list_ip)
+
+
